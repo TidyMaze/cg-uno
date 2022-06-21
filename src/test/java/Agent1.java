60fb1413cd1833b3c29a7d62253df2a7f233f0f4
Yann Rolland <yann.rolland@iadvize.com>
2022-06-21 21:03:32 +0200

    wip

--- src/test/java/Agent1.java
+++ src/test/java/Agent1.java
@@ -1,12 +1,21 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Agent1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Random random = new Random();
+
         while (true) {
-            String input = scanner.nextLine();
-            System.out.println("my output");
+            int handCount = Integer.parseInt(scanner.nextLine());
+
+            List<String> cards = new ArrayList<>();
+            for (int i = 0; i < handCount; i++) {
+                String card = scanner.nextLine();
+                cards.add(card);
+            }
+
+            System.out.println(cards.get(random.nextInt(cards.size())));
         }
     }
 }
