507081601c40ff358541e9479a0da3690b67cfa2
Yann Rolland <yann.rolland@iadvize.com>
2022-06-22 01:13:55 +0200

    wip

--- src/main/java/com/codingame/game/State.java
+++ src/main/java/com/codingame/game/State.java
@@ -33,6 +33,8 @@ public class State {
 
     public List<Card> draw(MultiplayerGameManager gameManager, int count) {
         if (deck.size() < count) {
+            System.out.println(String.format("Deck is too small (%d < %d), drawing from discard pile (%d)", deck.size(), count, discardPile.size()));
+
             List<Card> allButLastDiscarded = new ArrayList<>(discardPile.subList(0, discardPile.size() - 1));
             deck.addAll(allButLastDiscarded);
             discardPile = new ArrayList<>(discardPile.subList(discardPile.size() - 1, discardPile.size()));
